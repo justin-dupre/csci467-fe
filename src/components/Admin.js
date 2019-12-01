75af240371dc1b827bae4831cc8791358bf5aa69
justin-dupre <justinnedits@gmail.com>
2019-12-01 06:07:17 -0600

    able to add new associate

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import check from '../images/check.png';
-import remove from '../images/remove.png'
+import remove from '../images/remove.png';
+import plus from '../images/plus.png';
 
 class Admin extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            selectedAssociate: {}
+            selectedAssociate: {},
+            newAssociate: {}
         }
     }
 
     openModal(associate) {
         console.log(associate);
-        
+
         this.setState({
             selectedAssociate: associate
         })
@@ -25,7 +27,7 @@ class Admin extends Component {
         this.setState({
             selectedAssociate: {
                 ...this.state.selectedAssociate,
-                [type]: type === "admin" ?  e.target.checked : (type === "commission" ? parseFloat(e.target.value) : e.target.value)
+                [type]: type === "admin" ? e.target.checked : (type === "commission" ? parseFloat(e.target.value) : e.target.value)
             }
         }, console.log(this.state.selectedAssociate)
         )
@@ -63,79 +65,161 @@ class Admin extends Component {
         })
 
     }
+
+    valueChangeAdd(e, type) {
+
+        this.setState({
+            newAssociate: {
+                ...this.state.newAssociate,
+                [type]: type === 'admin' ? e.target.checked : e.target.value
+
+            }
+
+        })
+    }
+
+    addAssociate() {
+
+        console.log(this.state.newAssociate);
+        if (!this.state.newAssociate.name === null || !this.state.newAssociate.password) {
+            window.alert('Failed to create new associate, please fill out all fields.')
+            return;
+        }
+
+
+
+        this.props.dispatch({
+            type: "ADD_ASSOCIATE",
+            payload: {
+                'name': this.state.newAssociate.name,
+                'commission': 0,
+                'id': Math.random().toString(36).substr(2, 9),
+                'password': this.state.newAssociate.password,
+                'admin': this.state.newAssociate.admin
+            }
+        });
+
+        this.forceUpdate();
+        window.alert('Successfully added associate. ' + this.state.newAssociate.name + ' is now able to access the application.')
+    }
+
+
     render() {
 
-        
-        
+
+
 
         if (this.props.associateLoggedIn.admin) {
             return (
-                <React.Fragment>
-                    <table class="table table-striped mt-5">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Commission</th>
-                                <th scope="col">Admin</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.associates.map((associate, i) => {
-                                return (
-                                    <tr className="myhover" data-toggle="modal" data-target="#exampleModal" onClick={() => this.openModal(associate)}>
-                                        <th className="truncate" scope="row">{associate.id} </th>
-                                        <td className="truncate">{associate.name}</td>
-                                        <td className="truncate">${associate.commission.toFixed(2)}</td>
-                                        <td className="truncate">{<img style={{ width: '25px' }} src={associate.admin ? check : remove} />}</td>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-5">
+                            <h2>Associate Info</h2>
+                            <span>Add Associate</span> <img data-toggle="modal" data-target="#addmodal" src={plus} style={{ width: '25px' }} />
+                            <table class="table table-striped mt-3">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Commission</th>
+                                        <th scope="col">Admin</th>
                                     </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                                </thead>
+                                <tbody>
+                                    {this.props.associates.map((associate, i) => {
+                                        return (
+                                            <tr className="myhover" data-toggle="modal" data-target="#exampleModal" onClick={() => this.openModal(associate)}>
+                                                <th className="truncate" scope="row">{associate.id} </th>
+                                                <td className="truncate">{associate.name}</td>
+                                                <td className="truncate">${associate.commission.toFixed(2)}</td>
+                                                <td className="truncate">{<img style={{ width: '25px' }} src={associate.admin ? check : remove} />}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
 
 
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Edit Info for {this.state.selectedAssociate.name}</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form class="">
-
-                                        <div className="mb-2 ml-2 form-group">
-                                            Name: <input onChange={(e) => this.valueChange(e, 'name')} type="text" value={this.state.selectedAssociate.name} />
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Edit Info for {this.state.selectedAssociate.name}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
                                         </div>
-                                        <div className="mb-2 ml-2 form-group">
-                                            Password: <input onChange={(e) => this.valueChange(e, 'password')} type="text" value={this.state.selectedAssociate.password} />
-                                        </div>
+                                        <div class="modal-body">
+                                            <form class="">
 
-                                        <div className="mb-2 ml-2 form-group">
-                                        Commission: <input onChange={(e) => this.valueChange(e, 'commission')} type="number" value={this.state.selectedAssociate.commission} />
-                                        </div>
+                                                <div className="mb-2 ml-2 form-group">
+                                                    Name: <input onChange={(e) => this.valueChange(e, 'name')} type="text" value={this.state.selectedAssociate.name} />
+                                                </div>
+                                                <div className="mb-2 ml-2 form-group">
+                                                    Password: <input onChange={(e) => this.valueChange(e, 'password')} type="text" value={this.state.selectedAssociate.password} />
+                                                </div>
 
-                                        <div class="form-check ml-2 form-group">
-                                            <input class="form-check-input" onChange={(e) => this.valueChange(e, 'admin')} type="checkbox" value="" id="sanctionCheck" checked={this.state.selectedAssociate.admin} />
-                                            <label class="form-check-label" for="defaultCheck1">
-                                                Admin
+                                                <div className="mb-2 ml-2 form-group">
+                                                    Commission: <input onChange={(e) => this.valueChange(e, 'commission')} type="number" value={this.state.selectedAssociate.commission} />
+                                                </div>
+
+                                                <div class="form-check ml-2 form-group">
+                                                    <input class="form-check-input" onChange={(e) => this.valueChange(e, 'admin')} type="checkbox" value="" id="sanctionCheck" checked={this.state.selectedAssociate.admin} />
+                                                    <label class="form-check-label" for="defaultCheck1">
+                                                        Admin
                                         </label>
-                                        </div>
-                                    </form>
+                                                </div>
+                                            </form>
 
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button onClick={() => this.saveChanges()} data-dismiss="modal" type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button onClick={() => this.saveChanges()} data-dismiss="modal" type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div class="modal fade" id="addmodal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" >Add new associate</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form class="">
+
+                                                <div className="mb-2 ml-2 form-group">
+                                                    Name: <input onChange={(e) => this.valueChangeAdd(e, 'name')} type="text" />
+                                                </div>
+                                                <div className="mb-2 ml-2 form-group">
+                                                    Password: <input onChange={(e) => this.valueChangeAdd(e, 'password')} type="text" />
+                                                </div>
+
+                                                <div class="form-check ml-2 form-group">
+                                                    <input class="form-check-input" onChange={(e) => this.valueChangeAdd(e, 'admin')} type="checkbox" value="" id="sanctionCheck" />
+                                                    <label class="form-check-label" for="defaultCheck1">
+                                                        Admin
+                                                    </label>
+                                                </div>
+                                            </form>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button onClick={() => this.addAssociate()} data-dismiss="modal" type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-
-                </React.Fragment>
+                </div>
             )
         } else {
             return (
--- src/reducers/associateReducer.js
+++ src/reducers/associateReducer.js
@@ -1,74 +1,82 @@
 const initialState = {
-    associates: [
-        {
-            'name':'Azra Roche',
-            'commission': 45.89,
-            'id': 'uwolw25z8',
-            'password': 'password',
-            'admin': false
-        },
-        {
-            'name':'Fredrick Reader',
-            'commission': 105.65,
-            'id': 'qnsa6xsjz',
-            'password': 'password',
-            'admin': false
-        },
-        {
-            'name':'Portia Smith',
-            'commission': 207.45,
-            'id': 'g3ju94bjk',
-            'password': 'password',
-            'admin': true
-        },
-        {
-            'name':'Martyna Hutchings',
-            'commission': 67.34,
-            'id': 'hy535n748',
-            'password': 'password',
-            'admin': false
-        },
-        {
-            'name':'Seth Hall',
-            'commission': 543.23,
-            'id': '4p5p8y9gi',
-            'password': 'password',
-            'admin': true
-        }
-    ]
-  };
-  
-  function associateReducer(state = initialState, action) {
-  
-    switch(action.type) {
-      case 'ADD_COMMISSON':
-          let newState = {...state};
-          newState.associates.forEach((associate, i) => {
-            if(associate.id === action.payload.id){
-              newState.associates[i].commission += action.payload.commission
-            }
-          });
-          return {
-            ...state,
-            associates: newState.associates
-          };
-      case 'EDIT_ASSOCIATE':
-
-          let newerState = {...state};
-          newerState.associates.forEach((associate, i) => {
-            if(associate.id === action.payload.id){
-              newerState.associates[i] = action.payload
-            }
-          });
-    
-          console.log(newerState.associates);
-        return {
-         ...state,
-         associates: newerState.associates
-        };
-      default:
-        return state;
+  associates: [
+    {
+      'name': 'Azra Roche',
+      'commission': 45.89,
+      'id': 'uwolw25z8',
+      'password': 'password',
+      'admin': false
+    },
+    {
+      'name': 'Fredrick Reader',
+      'commission': 105.65,
+      'id': 'qnsa6xsjz',
+      'password': 'password',
+      'admin': false
+    },
+    {
+      'name': 'Portia Smith',
+      'commission': 207.45,
+      'id': 'g3ju94bjk',
+      'password': 'password',
+      'admin': true
+    },
+    {
+      'name': 'Martyna Hutchings',
+      'commission': 67.34,
+      'id': 'hy535n748',
+      'password': 'password',
+      'admin': false
+    },
+    {
+      'name': 'Seth Hall',
+      'commission': 543.23,
+      'id': '4p5p8y9gi',
+      'password': 'password',
+      'admin': true
     }
+  ]
+};
+
+function associateReducer(state = initialState, action) {
+
+  switch (action.type) {
+    case 'ADD_COMMISSON':
+      let newState = { ...state };
+      newState.associates.forEach((associate, i) => {
+        if (associate.id === action.payload.id) {
+          newState.associates[i].commission += action.payload.commission
+        }
+      });
+      return {
+        ...state,
+        associates: newState.associates
+      };
+    case 'EDIT_ASSOCIATE':
+
+      let newerState = { ...state };
+      newerState.associates.forEach((associate, i) => {
+        if (associate.id === action.payload.id) {
+          newerState.associates[i] = action.payload
+        }
+      });
+
+      console.log(newerState.associates);
+      return {
+        ...state,
+        associates: newerState.associates
+      };
+    case 'ADD_ASSOCIATE':
+      let oldState = {...state}
+      oldState.associates.push(action.payload)
+
+      return {
+        ...state,
+        associates: oldState.associates
+      };
+    default:
+      return state;
   }
-  
-  export default associateReducer;
+}
+
+export default associateReducer;
